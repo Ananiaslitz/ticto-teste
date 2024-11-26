987d3a7b85b667decb29c117900b349b9d17dd44
Diego Ananias <diego.ananias@ifood.com.br>
2024-11-26 20:39:37 -0300

    chore(config): register RegisterPointCommand and handler in commands.php

--- config/commands.php
+++ config/commands.php
@@ -1,3 +1,8 @@
 <?php
 
-return [];
+use Core\Application\Commands\RegisterPointCommand;
+use Core\Application\Handlers\RegisterPointHandler;
+
+return [
+    RegisterPointCommand::class => RegisterPointHandler::class,
+];
